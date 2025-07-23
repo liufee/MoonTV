8ab0e328ad16173551040549b638fdf0d56102d9
liufee <job@feehi.com>
2025-07-23 08:02:49 +0800

    feat: no announcement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -297,7 +297,7 @@ function HomeClient() {
           )}
         </div>
       </div>
-      {announcement && showAnnouncement && (
+      {false && (
         <div
           className={`fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm dark:bg-black/70 p-4 transition-opacity duration-300 ${
             showAnnouncement ? '' : 'opacity-0 pointer-events-none'
