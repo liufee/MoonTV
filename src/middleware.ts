a3438ea28ff4dfdafa20b66a4aae7e49c2b0aeec
liufee <job@feehi.com>
2025-08-01 10:43:45 +0800

    feat: close wanring

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,8 +16,8 @@ export async function middleware(request: NextRequest) {
 
   if (!process.env.PASSWORD) {
     // 如果没有设置密码，重定向到警告页面
-    const warningUrl = new URL('/warning', request.url);
-    return NextResponse.redirect(warningUrl);
+    //const warningUrl = new URL('/warning', request.url);
+    //return NextResponse.redirect(warningUrl);
   }
 
   // 从cookie获取认证信息
